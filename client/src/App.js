a3cdfcd98b3453b12b3df9ba2d28ea05cd81512e
RishanRatna <rishanratna@hotmail.com>
2020-11-06 11:43:51 -0500

    Create context api and reducer template

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import ContactState from './context/contacts/ContactState';
 
 const App = () => {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <div>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
+    <ContactState>
+      <Router>
+        <div className="container">
+            <Navbar />
+            <div>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ContactState>
   );
 }
 
